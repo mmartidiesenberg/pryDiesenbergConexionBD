13792d22726a01fea22634e57c4dc776074c5fa8
Alumno <Alumno@PC115>
2026-03-26 09:11:53 -0300

    Agregar archivos de proyecto.

---
+++ pryDiesenbergConexionBD/ClassConexionBD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+
+namespace pryDiesenbergConexionBD
+{
+    internal class ClassConexionBD
+    {
+        OleDbConnection conn;
+        
+        public void ConectarBD()
+        {
+            conn = new OleDbConnection();
+
+            conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Application.StartupPath + "\\..\\..\\BaseDatos\\baseJuegoRPG.accdb";
+
+            conn.Open();
+
+        }
+
+
+    }
+}
